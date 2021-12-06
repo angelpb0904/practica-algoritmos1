4cf93be8f2f866f0d9f06260d3252e54146b385e
unknown <angelpb0904@gmail.com>
2021-12-06 07:23:05 -0600

    ejercicios

---
+++ ejer2.js
@@ -0,0 +1,5 @@
+/* Haga un programa al cual se le digite un número entero y 
+despliegue por pantalla la suma de  los números comprendidos entre 0 y el número digitado. */
+
+const a = prompt("Introduce tu primer numero positivo:");
+const b = prompt("Introduce tu segundo numero positivo");
