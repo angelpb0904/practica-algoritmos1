49e00f693d97924467ce6eff8fecf07cd60b732b
unknown <angelpb0904@gmail.com>
2021-12-18 16:54:31 -0600

    16

---
+++ ejer16.js
@@ -0,0 +1,5 @@
+/* Escriba un programa que solicite por pantalla el número de horas trabajadas en una semana  y el salario 
+por hora de un empleado. A continuación, imprima el salario bruto, las  retenciones y el salario neto. Suponga la siguiente información: 
+(a) Horas extras (por encima de 40), se pagan a razón de 1.5* salario por hora. 
+(b) Retenciones: 10% de los primeros RD$1100; 15% de los 1500 siguientes y 25% del  resto. */
+
