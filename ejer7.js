43a7783de6c03f347537824a7eaee3c5403c25de
unknown <angelpb0904@gmail.com>
2021-12-18 16:48:40 -0600

    7

--- ejer7.js
+++ ejer7.js
@@ -6,25 +6,11 @@ no muere, ¿Cuántos pares de conejos se  tendrán al cabo de un año?. */
 
 const fibonacci = (n) => {
 
-    // Ejemplo de fibonnacci 1, 1, 2, 3, 5, 8, 13
-
-    //  f = f(n-1) + f(n-2)
-
-    // Base case
     if (n < 2) {
         return n;
     }
 
-    // Recursive case
-    // 4  3
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
-
-// Rama con 6
-// rama de 5 y rama 4
-
-// Rama con 5
-// rama de 4 y rama 3
-
 
 fibonacci(7);
