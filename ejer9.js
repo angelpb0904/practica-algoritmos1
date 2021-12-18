9aa2ee52f98caf6bf4df5bf55a8296c213c3d350
unknown <angelpb0904@gmail.com>
2021-12-18 16:50:09 -0600

    9

---
+++ ejer9.js
@@ -0,0 +1,11 @@
+/*Dada una matriz de números positivos y un número positivo "k", encuentre la suma máxima de cualquier submatriz contigua de tamaño "k".
+Input: [2, 1, 5, 1, 3, 2],  k=3 
+Output: 9
+Resultado con la máxima suma es [5, 1, 3] = 5+1+3 = 9. */
+
+let imput = [2, 1, 5, 1, 3, 2];
+let k = 3;
+var suma = 0;
+var max = 0;
+
+
