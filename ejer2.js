999ef7cf7da7605a91b1fac872d8bfc4a15d1f50
unknown <angelpb0904@gmail.com>
2021-12-18 16:45:54 -0600

    2

--- ejer2.js
+++ ejer2.js
@@ -1,5 +1,20 @@
 /* Haga un programa al cual se le digite un número entero y 
 despliegue por pantalla la suma de  los números comprendidos entre 0 y el número digitado. */
 
-const a = prompt("Introduce tu primer numero positivo:");
-const b = prompt("Introduce tu segundo numero positivo");
+// Leer numero 
+let num = 9;
+let sum = 0;
+
+// not !
+while( !(num == 0)   ){
+    sum += num;
+    --num;
+}
+
+console.log(sum);
+
+
+// Gauss
+num = 9;
+let sum_gauss = num * (num + 1)/2
+console.log(sum_gauss);
