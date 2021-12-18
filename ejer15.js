2d717b90998dbd221ddbd59e79a6908c4ee16935
unknown <angelpb0904@gmail.com>
2021-12-18 16:53:18 -0600

    15

---
+++ ejer15.js
@@ -0,0 +1,3 @@
+/* Escriba un programa al cual se le digiten diez valores y al final imprima por pantalla la suma  de los 
+valores, las sumas de los cuadrados, el promedio, el máximo y el mínimo. */
+
